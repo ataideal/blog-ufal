56294b221c3963f236c67128ff916f0053697a80
Lucas Peixoto de Almeida Cavalcante <lpdac@ic.ufal.br>
2017-12-16 18:57:56 -0300

    Publication detail as a generic controller for site and masonry.

--- app/controllers/pub_detail_controller.rb
+++ app/controllers/pub_detail_controller.rb
@@ -1,4 +1,4 @@
-class Site::PubDetailController < SiteController
+class PubDetailController < SiteController
     def show
         @pub = Pub.find(params[:id])
         @tags = @pub.tag_list
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
 
   namespace :site do
     get 'home/index'
-    resources :pub_detail, only: [:show]
     resources :categories, only: [:show]
   end
 
@@ -15,10 +14,10 @@ Rails.application.routes.draw do
     resources :tags
     resources :categories
     root to: "users#index"
-
   end
 
   root to: 'site/home#index'
   devise_for :users
   resources :users
+  resources :pub_detail, only: [:show]
 end
