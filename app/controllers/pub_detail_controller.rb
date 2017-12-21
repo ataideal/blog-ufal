aff69a2dd6b174e23184a629bda8500da9988ba5
fabriond <gfbf@ic.ufal.br>
2017-12-20 23:15:49 -0300

    Added Recommendations

--- app/controllers/pub_detail_controller.rb
+++ app/controllers/pub_detail_controller.rb
@@ -2,5 +2,6 @@ class PubDetailController < SiteController
     def show
         @pub = Pub.find(params[:id])
         @tags = @pub.tag_list
+        @recommended = @pub.find_related_tags.first(6)
     end
 end
