fa439c77d18c93b2850cb7c4d61d22953884d79e
fabriond <gfbf@ic.ufal.br>
2017-12-21 02:01:30 -0300

    Added tag functionality to site/home/index

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,14 +2,14 @@ class Site::HomeController < SiteController
   before_action :set_pub, only: [:show]
 
   def index
-    if(params[:tag_list]) #tags with spaces must be written as ?tag_list="tag name"
+    if(params[:tag_list]) #tags with spaces must be written as ?tag_list="tag name" or ?tag_list=tag+name
       params[:tag_list].tr!("[]", "") #this line allows the use of brackets, like so: ?tag_list=["tag1"]
-      params[:tag_list] = params[:tag_list].split(",") #this line allows the use of multiple tags, like so: ?tag_list=[tag1,"tag two"] or ?tag_list=tag1,"tag two"
+      params[:tag_list] = params[:tag_list].split(",") #this line allows the use of multiple tags, like so: ?tag_list=[tag1,"tag two"] or ?tag_list=tag1,tag+two
       @pubs = Pub.tagged_with(params[:tag_list], any: true).page params[:page]
     else
       @pubs = Pub.all.page params[:page]
     end
-    @tags = ActsAsTaggableOn::Tag.most_used(10)
+    @tags = ActsAsTaggableOn::Tag.most_used(10).map(&:name)
   end
 
   def show
