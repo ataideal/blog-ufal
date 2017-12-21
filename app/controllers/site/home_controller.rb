3411b994b31bdd03aebd00bc150537184f0fce05
fabriond <gfbf@ic.ufal.br>
2017-12-20 23:17:35 -0300

    Added Listing of Posts by Tag

--- app/controllers/site/home_controller.rb
+++ app/controllers/site/home_controller.rb
@@ -2,14 +2,19 @@ class Site::HomeController < SiteController
   before_action :set_pub, only: [:show]
 
   def index
-    @pubs = Pub.all.page params[:page]
+    if(params[:tag_list]) #tags with spaces must be written as ?tag_list="tag name"
+      params[:tag_list].tr!("[]", "") #this line allows the use of brackets, like so: ?tag_list=["tag1"]
+      params[:tag_list] = params[:tag_list].split(",") #this line allows the use of multiple tags, like so: ?tag_list=[tag1,"tag two"] or ?tag_list=tag1,"tag two"
+      @pubs = Pub.tagged_with(params[:tag_list], any: true).page params[:page]
+    else
+      @pubs = Pub.all.page params[:page]
+    end
     @tags = ActsAsTaggableOn::Tag.most_used(10)
   end
 
   def show
     @tags = @pub.tag_list
   end
-
 
   private
   def set_pub
